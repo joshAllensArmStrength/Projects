29189ea123d102f63ca916a7f81f6b056a4612fd
Nick C <nick@gMacbook-Air.buffalo.rr.com>
2023-06-19 20:37:32 -0400

    Weather App Project Started

---
+++ Weather App/data_miner.py
@@ -0,0 +1,3 @@
+'''
+This class will serve as the foundation for our weather app. We will use the website 'OpenWeatherMap' API in order to grab our weather forecast data.
+'''
---
+++ Weather App/weatherAppGUI.py
@@ -0,0 +1,4 @@
+'''
+This will be the GUI section of our weather app. It will feature a 'home' page, where the user will be able to enter a city/town or a zip code, and the weekly forecast will then be displayed under the 'weekly forecast screen'. From this screen, the user will also be able to click onto a specific day, and will be brought to a new 'daily forecast' screen. This 'daily forecast' screen will show a more detailed look at the forecast for that day. 
+'''
+
