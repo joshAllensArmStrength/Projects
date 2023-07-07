4617dbe23cf34ea5381abe09fd7ebd8a9a60a9ea
Nick C <nick@gMacbook-Air.buffalo.rr.com>
2023-07-07 12:53:20 -0400

    Update for display weather screen

--- Weather App/weatherAppGUI.py
+++ Weather App/weatherAppGUI.py
@@ -5,6 +5,7 @@ This will be the GUI section of our weather app. It will feature a 'home' page, 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QLineEdit, QStackedWidget, QMainWindow
 from PyQt5.QtCore import Qt
+from getWeather import *
 
 class weatherGUI(QMainWindow):
     def __init__(self):
@@ -36,7 +37,8 @@ class weatherGUI(QMainWindow):
     def showSearchWeatherScreen(self):
         self.stacked_widget.setCurrentWidget(self.findWeather_screen)
         
-    def showDisplayWeatherScreen(self):
+    def showDisplayWeatherScreen(self, zipcode):
+        self.displayWeather_screen.setZipCode(zipcode)  
         self.stacked_widget.setCurrentWidget(self.displayWeather_screen)
         
     def showAboutScreen(self):
@@ -77,17 +79,90 @@ class FindWeatherScreen(QWidget):
     def __init__(self,parent):
         super().__init__()
         self.parent = parent
-        #self.setUI()
+        self.setUI()
+        
+        
+    def setUI(self):
+        self.search_label = QLabel('Enter Zip Code Below:')
+        self.search_label.setAlignment(Qt.AlignCenter)
+        
+        self.location_input = QLineEdit()
+        self.location_input.setStyleSheet("font-size: 18pt;")
+        
+        self.searchWeather_button = QPushButton('Search Weather')
+        self.searchWeather_button.setFixedSize(200,50)
+        self.searchWeather_button.clicked.connect(self.findWeather) 
+        
+        self.home_button = QPushButton('Home')
+        self.home_button.setFixedSize(200,50)
+        self.home_button.clicked.connect(self.parent.showHomeScreen)
+        
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.searchWeather_button)
+        button_layout.addWidget(self.home_button)
+        
+        findWeather_layout = QVBoxLayout()
+        findWeather_layout.addWidget(self.search_label)
+        findWeather_layout.addWidget(self.location_input)
+        findWeather_layout.addLayout(button_layout)
+        
+        self.setLayout(findWeather_layout)
+        
+        
+    def findWeather(self):
+        zipCode = self.location_input.text()
+        self.parent.showDisplayWeatherScreen(zipCode)
+        
         
         
 
 #-----------------------------------------------------
 
 class DisplayWeatherScreen(QWidget):
+    
+    zipCode = ''
+    
     def __init__(self,parent):
         super().__init__()
         self.parent = parent
-        #self.setUI()
+    
+    def setUI(self):  
+        api_key = '3d457d463e8f2a568c8717d8e9947002'
+        weatherInfo = getWeather(api_key,'',self.zipCode)
+        
+        # Qlabels for all of the weather info
+        self.location_label = QLabel(str(weatherInfo.location))
+        self.tempH_label = QLabel(str(weatherInfo.temp_high))
+        self.tempL_label = QLabel(str(weatherInfo.temp_low))
+        self.windSpeed_label = QLabel(str(weatherInfo.wind_speed))
+        self.humidity_label = QLabel(str(weatherInfo.humidity))
+        self.description_label = QLabel(str(weatherInfo.description))
+        self.sunrise_label = QLabel(str(weatherInfo.sunrise))
+        self.sunset_label = QLabel(str(weatherInfo.sunset))
+        
+        # Create vertical layout and add weather info labels
+        weather_layout = QVBoxLayout()
+        weather_layout.addWidget(self.location_label)
+        weather_layout.addWidget(self.tempH_label)
+        weather_layout.addWidget(self.tempL_label)
+        weather_layout.addWidget(self.windSpeed_label)
+        weather_layout.addWidget(self.humidity_label)
+        weather_layout.addWidget(self.description_label)
+        weather_layout.addWidget(self.sunrise_label)
+        weather_layout.addWidget(self.sunset_label)
+        
+        self.back_button = QPushButton('Go Back')
+        self.back_button.clicked.connect(self.parent.showSearchWeatherScreen)
+        
+        weather_layout.addWidget(self.back_button)
+        
+        self.setLayout(weather_layout)
+        
+        
+        
+    def setZipCode(self,zipCode):
+        self.zipCode = zipCode
+        self.setUI()
         
 
 #-----------------------------------------------------
